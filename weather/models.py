9362cd1e1d7e437b58a6cbb45df2c3bb5761fa79
Morrisa Brenner <morrisab@mit.edu>
2018-08-28 22:44:29 -0400

    Additional database configuration stuff

--- weather/models.py
+++ weather/models.py
@@ -5,5 +5,5 @@ from django.db import models
 
 class Location(models.Model):
     zip = models.IntegerField()
-    lat = models.DecimalField(max_digits=8, decimal_places=5, null=None)
-    lon = models.DecimalField(max_digits=8, decimal_places=5, null=None)
+    lat = models.DecimalField(max_digits=8, decimal_places=5, null=True)
+    lon = models.DecimalField(max_digits=8, decimal_places=5, null=True)
