440360a2cd89a5d31d27630925202986b9c307be
Morrisa Brenner <morrisab@mit.edu>
2018-08-28 22:37:24 -0400

    Additional database configuration stuff

--- rainbow_watch/settings.py
+++ rainbow_watch/settings.py
@@ -82,7 +82,8 @@ WSGI_APPLICATION = 'rainbow_watch.wsgi.application'
 
 DATABASES = {
     'default': {
-        # dj_database_url.config(conn_max_age=600, ssl_require=True)
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -125,9 +126,5 @@ STATIC_URL = '/static/'
 django_heroku.settings(locals())
 
 GEONAMES_USER = os.environ['GEONAMES_USER']
-
-DATABASE_URL = os.environ['DATABASE_URL']
-
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
