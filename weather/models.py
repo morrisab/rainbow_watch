2c4d1b792ea5673ca618e4aa16198539796460ba
Morrisa Brenner <morrisab@mit.edu>
2018-09-24 00:40:14 -0400

    Database formatting update

--- weather/models.py
+++ weather/models.py
@@ -4,6 +4,6 @@ from django.db import models
 # Create your models here.
 
 class Location(models.Model):
-    zip = models.IntegerField()
+    zip = models.CharField(max_length=5)
     lat = models.DecimalField(max_digits=8, decimal_places=5, null=True)
     lon = models.DecimalField(max_digits=8, decimal_places=5, null=True)
